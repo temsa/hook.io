943a788206f64558a46491f82e873a4069d720f3
Florian Traverse <florian.traverse@gmail.com>
2011-09-15 15:04:32 +0200

    Fixes for wildcard like 0.0.0.0 or :: on host for server

--- test/discovery/client-details-test.js
+++ test/discovery/client-details-test.js
@@ -5,7 +5,6 @@
  * MIT LICENCE
  *
  */
-
 var vows = require('vows'),
     assert = require('assert'),
     Hook = require('../../lib/hookio').Hook,
@@ -18,7 +17,7 @@ vows.describe('hook.io/discovery/client-details').addBatch({
       var self = this;
       
       server.on('hook::listening', function() {
-        var client1 = new Hook({ name: 'client', type: 'test'});
+        var client1 = new Hook({ name: 'client1', type: 'test'});
         client1.start({"hook-port":5011});
         
         var client2 = new Hook({ name: 'another-client', type: 'another-test'});
@@ -29,48 +28,61 @@ vows.describe('hook.io/discovery/client-details').addBatch({
         client2.start({"hook-port":5011});
       });
       server.start({"hook-port":5011});
-    },
-    "and a *client hook* emits *hookDetails* asking for details": {
-      "about *itself* by *name*": checkDetails(function (err, server, client, client2) {
-          var self = this;
-          client.emit('hookDetails', {name : client.name, callback: function onDetails (err, details) {
-            self.callback(err, details, client);
-          }});
-      }),
-      "about the *server* by *name*": checkDetails(function (err, server, client, client2) {
-          var self = this;
-          client.emit('hookDetails', {name : server.name, callback: function onDetails (err, details) {
-            self.callback(err, details, client);
-          }});
-      }),
-      "about all hooks of type *test*": checkMultipleDetails(function (err, server, client, client2) {
-          var self = this;
-          client.emit('hookDetails', {type : 'test', callback: function onDetails (err, details) {
-            self.callback(err, details, client);
-          }});
-      }, ['server','client']),
-      "about all hooks on host *127.0.0.1*": checkMultipleDetails(function (err, server, client, client2) {
-          var self = this;
-          client.emit('hookDetails', {host : '127.0.0.1', callback: function onDetails (err, details) {
-            self.callback(err, details, client);
-          }});
-      }, ['server','client', 'another-client']),
-      "about all hooks on host *localhost*": checkMultipleDetails(function (err, server, client, client2) {
-          var self = this;
-          client.emit('hookDetails', {host : 'localhost', callback: function onDetails (err, details) {
-            self.callback(err, details, client);
-          }});
-      }, ['server','client', 'another-client'])
-    },
+    },/*
+*/
     "and the *server hook* emits *hookDetails* asking for details": {
-      "about *itself* by *name*": checkDetails(function (err, server, client, client2) {
+      "about *itself* by *name*": checkDetails(function (server, client, client2) {
           var self = this;
           server.emit('hookDetails', {name : server.name, callback: function onDetails (err, details) {
-            self.callback(err, details, server);
+            self.callback(err, details, server, client, client2);
+          }});
+      }),
+			"about *the 1st client* by *name*": checkDetails(function (server, client, client2) {
+          var self = this;
+					server.emit('hookDetails', {name : client.name, callback: function onDetails (err, details) {
+            self.callback(err, details, server, client, client2);
+          }});
+      }),
+			"about *the 2nd client* by *name*": checkDetails(function (server, client, client2) {
+          var self = this;
+          server.emit('hookDetails', {name : client2.name, callback: function onDetails (err, details) {
+            self.callback(err, details, server, client, client2);
           }});
       })
+    },
+		"and a *client hook* emits *hookDetails* asking for details": {
+      "about *itself* by *name*": checkDetails(function (server, client, client2) {
+          var self = this;
+          client.emit('hookDetails', {name : client.name, callback: function onDetails (err, details) {
+            self.callback(err, details, server, client, client2);
+          }});
+      }),
+      "about the *server* by *name*": checkDetails(function (server, client, client2) {
+          var self = this;
+          client.emit('hookDetails', {name : server.name, callback: function onDetails (err, details) {
+            self.callback(err, details, server, client, client2);
+          }});
+      }),
+      "about all hooks of type *test*": checkMultipleDetails(function (server, client, client2) {
+          var self = this;
+          client.emit('hookDetails', {type : 'test', callback: function onDetails (err, details) {
+            self.callback(err, details, server, client, client2);
+          }});
+      }, ['server','client1']),
+      "about all hooks on host *127.0.0.1*": checkMultipleDetails(function (server, client, client2) {
+          var self = this;
+          client.emit('hookDetails', {host : '127.0.0.1', callback: function onDetails (err, details) {
+            self.callback(err, details, server, client, client2);
+          }});
+      }, ['server','client1', 'another-client']),
+      "about all hooks on host *localhost*": checkMultipleDetails(function (server, client, client2) {
+          var self = this;
+          client.emit('hookDetails', {host : 'localhost', callback: function onDetails (err, details) {
+            self.callback(err, details, server, client, client2);
+          }});
+      }, ['server','client1', 'another-client'])
     }
-  }
+	}
 }).export(module);
 
 
@@ -80,16 +92,16 @@ function checkDetails (topic) {
   return { 
     topic: topic,
     
-    "the callback should get the instance details": function (err, details, client) {
+    "the callback should get the instance details": function (err, details, server, client, client2) {
       assert.isObject(details);
     },
-    "details should contain the name": function (err, details, client) {
+    "details should contain the name": function (err, details, server, client, client2) {
       assert.isString(details.name);
     },
-    "details should contain the type": function (err, details, client) {
+    "details should contain the type": function (err, details, server, client, client2) {
       assert.isString(details.type);
     },
-    "details should contain the host": function (err, details, client) {
+    "details should contain the host": function (err, details, server, client, client2) {
       assert.isString(details.remote.host);
     }
   };
@@ -101,23 +113,23 @@ function checkMultipleDetails (topic, targets) {
   return  {
     topic: topic,
     
-    "the callback should get the instance details": function (err, multipleDetails, client) {
+    "the callback should get the instance details": function (err, multipleDetails, server, client, client2) {
       assert.isArray(multipleDetails);
     },
-    "the details should contain the good number of hooks": function (err, multipleDetails, client) {
+    "the details should contain the good number of hooks": function (err, multipleDetails, server, client, client2) {
       assert.strictEqual(multipleDetails.length, targets.length );
     },
-    "details should contain the name": function (err, multipleDetails, client) {
+    "details should contain the name": function (err, multipleDetails, server, client, client2) {
       multipleDetails.forEach(function(details) {
         assert.isString(details.name);
       });
     },
-    "details should contain the type": function (err, multipleDetails, client) {
+    "details should contain the type": function (err, multipleDetails, server, client, client2) {
       multipleDetails.forEach(function(details) {
         assert.isString(details.type);
       });
     },
-    "details should contain the host": function (err, multipleDetails, client) {
+    "details should contain the host": function (err, multipleDetails, server, client, client2) {
       multipleDetails.forEach(function(details) {
         assert.isString(details.remote.host);
       });
